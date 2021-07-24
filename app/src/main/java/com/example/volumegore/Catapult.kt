1a7bbc87ca76e18b6b338b050c096a79368643cc
Michael Friend <michaelf@touchlab.co>
2021-07-23 20:11:37 -0400

    Accellerated easing

--- app/src/main/java/com/example/volumegore/Catapult.kt
+++ app/src/main/java/com/example/volumegore/Catapult.kt
@@ -1,37 +1,29 @@
 package com.example.volumegore
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Circle
-import androidx.compose.material.icons.filled.RingVolume
 import androidx.compose.material.icons.filled.VolumeUp
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
@@ -40,18 +32,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import com.example.volumegore.VolumeChanged
 import com.example.volumegore.ui.theme.volumeGoreTheme
-import kotlinx.coroutines.delay
 import kotlin.math.roundToInt
 
 val redToGreen = keyframes<Color> {
     durationMillis = 6_000
-    Color.Green at 0 with FastOutSlowInEasing
-    Color.Yellow at 4_000 with FastOutSlowInEasing
-    Color.Red at 6_000 with FastOutSlowInEasing
+    Color.Green at 0 with FastOutLinearInEasing
+    Color.Yellow at 4_000 with FastOutLinearInEasing
+    Color.Red at 6_000 with FastOutLinearInEasing
 }
-
 @Composable
 fun CatapultVolume(volume: Int, volumeChanged: VolumeChanged) {
     ConstraintLayout(
@@ -61,7 +50,12 @@ fun CatapultVolume(volume: Int, volumeChanged: VolumeChanged) {
     ) {
         val (image, slider, angle, ball) = createRefs()
         val charging = remember { mutableStateOf(false) }
-        val rotate by animateFloatAsState(targetValue = if (charging.value) 45f else 0f, tween(6_000))
+        // infiniteRepeatable will continue to repeat until we recompose with a different animation, so switch to a tween
+        // when the button is released
+        val rotate by animateFloatAsState(
+            targetValue = if (charging.value) 45f else 0f,
+            if (charging.value) infiniteRepeatable(tween(6_000, easing =FastOutLinearInEasing), RepeatMode.Reverse) else tween(6_000)
+        )
         val color by animateColorAsState(
             targetValue = if (charging.value) Color.Red else Color.Black,
             if (charging.value) redToGreen else snap()
