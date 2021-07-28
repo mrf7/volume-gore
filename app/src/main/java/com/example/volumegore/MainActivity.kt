9afaba5c067a009a9d8d528775b1ffc41b8a3e96
Michael Friend <michaelf@touchlab.co>
2021-07-28 17:13:35 -0400

    remove imports

--- app/src/main/java/com/example/volumegore/MainActivity.kt
+++ app/src/main/java/com/example/volumegore/MainActivity.kt
@@ -3,34 +3,10 @@ package com.example.volumegore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Modifier
 import com.example.volumegore.ui.theme.volumeGoreTheme
-import com.example.volumegore.volumescreens.SevenSegment
 
 typealias VolumeChanged = (volume: Int) -> Unit
 
